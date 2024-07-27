6cf7ccf69ea2a8624d9175c6a2f5625d31badfee
gild-icy <gild@icyghost.com>
2024-07-27 12:21:16 +0300

    update

--- src/main/java/com/coreApplication/Model/DLModel.java
+++ src/main/java/com/coreApplication/Model/DLModel.java
@@ -1,44 +1,17 @@
 package com.coreApplication.Model;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DLModel {
 
     public DLModel() {
-        setupVirtualEnvironment();
-    }
-
-    private void setupVirtualEnvironment() {
-        String venvPath = "src/main/python/venv";
-        File venvDir = new File(venvPath);
-        if (!venvDir.exists()) {
-            try {
-                // Create the virtual environment
-                ProcessBuilder venvBuilder = new ProcessBuilder("/bin/zsh", "-c", "python3 -m venv " + venvPath);
-                venvBuilder.directory(new File("/"));
-                venvBuilder.start().waitFor();
-
-                // Install required packages
-                String[] packages = {"torch", "transformers"}; // Example packages
-                for (String pkg : packages) {
-                    ProcessBuilder pipBuilder = new ProcessBuilder("/bin/zsh", "-c", venvPath + "/bin/pip install " + pkg);
-                    pipBuilder.start().waitFor();
-                }
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public float predict(String text) {
         try {
-            String command = "/bin/zsh -c \"source src/main/python/venv/bin/activate && python src/main/python/modelPredictionScript.txt \\\"" + text.replace("\"", "\\\"") + "\\\"\"";
-
-            ProcessBuilder pb = new ProcessBuilder("/bin/zsh", "-c", command);
-            Process p = pb.start();
+            Process p = getProcess(text);
 
             int exitCode = p.waitFor();
             if (exitCode == 0) {
@@ -51,12 +24,30 @@ public class DLModel {
                     return -1;
                 }
             } else {
-                System.err.println("Python script execution failed with exit code " + exitCode);
+                BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                String errorLine;
+                while ((errorLine = error.readLine()) != null) {
+                    System.err.println(errorLine);
+                }
                 return -1;
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return -1;
         }
+    }
+
+    private static Process getProcess(String text) throws IOException {
+        // In Order to run the virtual environment:
+        // 1. In the console write the following:
+        // a. python3 -m venv venv    
+        // b. source venv/bin/activate
+        // c. pip install torch transformers
+        // 2. Change the absolutePath to the path of the virtual environment in the project
+        String absolutePath = "/Users/user/Documents/GitHub/DL-Project/venv/bin/activate";
+        String command = "/bin/zsh -c \"source " + absolutePath + " && python src/main/python/modelPredictionScript.txt \\\"" + text.replace("\"", "\\\"") + "\\\"\"";
+        ProcessBuilder pb = new ProcessBuilder("/bin/zsh", "-c", command);
+        Process p = pb.start();
+        return p;
     }
 }
